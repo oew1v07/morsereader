8817ed23e0cd97d02e5806fbf24cfe6ea3e35dab
Robin Wilson <robin@rtwilson.com>
2013-08-24 20:24:08 +0100

    Button state change working on hardware

--- morsereader/morsereader.ino
+++ morsereader/morsereader.ino
@@ -1,88 +1,56 @@
-/* 
- Debounce
- 
- Each time the input pin goes from LOW to HIGH (e.g. because of a push-button
- press), the output pin is toggled from LOW to HIGH or HIGH to LOW.  There's
- a minimum delay between toggles to debounce the circuit (i.e. to ignore
- noise).  
- 
- The circuit:
- * LED attached from pin 13 to ground
- * pushbutton attached from pin 2 to +5V
- * 10K resistor attached from pin 2 to ground
- 
- * Note: On most Arduino boards, there is already an LED on the board
- connected to pin 13, so you don't need any extra components for this example.
- 
- 
- created 21 November 2006
- by David A. Mellis
- modified 30 Aug 2011
- by Limor Fried
- modified 28 Dec 2012
- by Mike Walters
- 
- This example code is in the public domain.
- 
- http://www.arduino.cc/en/Tutorial/Debounce
- */
-
-// constants won't change. They're used here to 
-// set pin numbers:
-const int buttonPin = 9;    // the number of the pushbutton pin
-const int ledPin = 13;      // the number of the LED pin
+// this constant won't change:
+const int  buttonPin = 9;    // the pin that the pushbutton is attached to
+const int ledPin = 13;       // the pin that the LED is attached to
 
 // Variables will change:
-int ledState = HIGH;         // the current state of the output pin
-int buttonState;             // the current reading from the input pin
-int lastButtonState = LOW;   // the previous reading from the input pin
-
-// the following variables are long's because the time, measured in miliseconds,
-// will quickly become a bigger number than can be stored in an int.
-long lastDebounceTime = 0;  // the last time the output pin was toggled
-long debounceDelay = 50;    // the debounce time; increase if the output flickers
+int buttonPushCounter = 0;   // counter for the number of button presses
+int buttonState = 0;         // current state of the button
+int lastButtonState = 0;     // previous state of the button
 
 void setup() {
+  // initialize the button pin as a input:
   pinMode(buttonPin, INPUT);
+  // initialize the LED as an output:
   pinMode(ledPin, OUTPUT);
-
-  // set initial LED state
-  digitalWrite(ledPin, ledState);
+  // initialize serial communication:
+  Serial.begin(9600);
 }
 
+
 void loop() {
-  // read the state of the switch into a local variable:
-  int reading = digitalRead(buttonPin);
+  // read the pushbutton input pin:
+  buttonState = digitalRead(buttonPin);
 
-  // check to see if you just pressed the button 
-  // (i.e. the input went from LOW to HIGH),  and you've waited 
-  // long enough since the last press to ignore any noise:  
-
-  // If the switch changed, due to noise or pressing:
-  if (reading != lastButtonState) {
-    // reset the debouncing timer
-    lastDebounceTime = millis();
-  } 
-  
-  if ((millis() - lastDebounceTime) > debounceDelay) {
-    // whatever the reading is at, it's been there for longer
-    // than the debounce delay, so take it as the actual current state:
-
-    // if the button state has changed:
-    if (reading != buttonState) {
-      buttonState = reading;
-
-      // only toggle the LED if the new button state is HIGH
-      if (buttonState == HIGH) {
-        ledState = !ledState;
-      }
+  // compare the buttonState to its previous state
+  if (buttonState != lastButtonState) {
+    // if the state has changed, increment the counter
+    if (buttonState == HIGH) {
+      // if the current state is HIGH then the button
+      // wend from off to on:
+      buttonPushCounter++;
+      Serial.println("on");
+      Serial.print("number of button pushes:  ");
+      Serial.println(buttonPushCounter);
+    } 
+    else {
+      // if the current state is LOW then the button
+      // wend from on to off:
+      Serial.println("off"); 
     }
   }
-  
-  // set the LED:
-  digitalWrite(ledPin, ledState);
+  // save the current state as the last state, 
+  //for next time through the loop
+  lastButtonState = buttonState;
 
-  // save the reading.  Next time through the loop,
-  // it'll be the lastButtonState:
-  lastButtonState = reading;
+  
+  // turns on the LED every four button pushes by 
+  // checking the modulo of the button push counter.
+  // the modulo function gives you the remainder of 
+  // the division of two numbers:
+  if (buttonPushCounter % 4 == 0) {
+    digitalWrite(ledPin, HIGH);
+  } else {
+   digitalWrite(ledPin, LOW);
+  }
+  
 }
